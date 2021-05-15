1163870602d12fcd80b80fc448c4675f3012daaa
Dinesh <dinesh729.dm@gmail.com>
2021-05-15 09:25:14 +0530

    cors handling and response changes

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE');
+
+	next();
+});
+
 app.use('/api/places',placesRoutes);
 
 app.use('/api/users',usersRoutes);
@@ -29,7 +40,7 @@ app.use((error,req,res,next)=>{
 
 mongoose
 	.connect(
-		'mongodb+srv://dinesh:zL6iRSuHPozyNClo@cluster0.9x4kv.mongodb.net/places?retryWrites=true&w=majority',
+		'mongodb+srv://dinesh:zL6iRSuHPozyNClo@cluster0.9x4kv.mongodb.net/mern?retryWrites=true&w=majority',
 		{ useUnifiedTopology: true }
 	)
 	.then(()=>{
